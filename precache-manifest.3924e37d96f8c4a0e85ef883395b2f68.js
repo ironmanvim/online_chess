ac03894c7fe3cd71fb30597fe60e2bb525bda386
C Vishal <vishalchiluveri@gmail.com>
2020-03-24 23:34:30 +0530

    Updates

---
+++ precache-manifest.3924e37d96f8c4a0e85ef883395b2f68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14feecbf91aa39587ffef962c53a6b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec8741b611ca1a2c7f7c",
+    "url": "/static/css/main.9a67afed.chunk.css"
+  },
+  {
+    "revision": "62405e673b6d35ab15ab",
+    "url": "/static/js/2.a5b69666.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a5b69666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8741b611ca1a2c7f7c",
+    "url": "/static/js/main.78746f8f.chunk.js"
+  },
+  {
+    "revision": "9e9fe82c18cb9f9ca480",
+    "url": "/static/js/runtime-main.061d122b.js"
+  }
+]);
